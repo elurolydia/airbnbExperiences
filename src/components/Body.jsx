c5cfb3d2de3b8613b5c854ecb52aa10e46dd3f3e
Lydia Eluro <elurolydia@gmail.com>
2022-08-11 22:08:50 -0500

    first commit

---
+++ src/components/Body.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import group from "./images/group.png";
+
+
+export default function Body () {
+    return (
+        <div>
+            <img src={group} alt="group images" />
+        </div>
+    )
+}
